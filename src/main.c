877f71b49d8c2be6b3603f95b0a3d88d236f0f7e
guipleite <guipleite@gmail.com>
2019-03-09 16:42:42 -0300

    botao de start , next e led indicando a musica

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /**
  * 5 semestre - Eng. da Computa��o - Insper
- /**
+
   ** Entrega realizada em parceria com:
   **  - Gabriel Monteiro - gabriellm1@al.insper.edu.br
   **  - Guilherme Leite -
@@ -28,6 +28,16 @@
 #define LED_PIO_ID        12                    // ID do perif�rico PIOC (controla LED)
 #define LED_PIO_IDX       8u                    // ID do LED no PIO
 #define LED_PIO_IDX_MASK  (1u << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+// defines do Botao de start
+#define BUTTSTART_PIO           PIOC
+#define BUTTSTART_PIO_ID        ID_PIOC
+#define BUTTSTART_PIO_IDX       17u
+#define BUTTSTART_PIO_IDX_MASK  (1u << BUTTSTART_PIO_IDX)
+// defines do Botao de next
+#define BUTTNEXT_PIO           PIOC
+#define BUTTNEXT_PIO_ID        ID_PIOC
+#define BUTTNEXT_PIO_IDX       30u
+#define BUTTNEXT_PIO_IDX_MASK  (1u << BUTTNEXT_PIO_IDX)
 
 
 void init(void);
@@ -52,11 +62,34 @@ void init(void)
 	  //Inicializa PA19 como sa�da
 	  pio_set_output(BUZZ_PIO, BUZZ_PIO_IDX_MASK, 1, 0, 0);
 
-	    // Ativa o PIO na qual o LED foi conectado
-	    // para que possamos controlar o LED.
-	    pmc_enable_periph_clk(LED_PIO_ID);
-	    //Inicializa PC8 como sa�da
-	    pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 1, 0, 0);
+	  // Ativa o PIO na qual o LED foi conectado
+	  // para que possamos controlar o LED.
+	  pmc_enable_periph_clk(LED_PIO_ID);
+	  //Inicializa PC8 como sa�da
+	  pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 1, 0, 0);
+		
+	 pmc_enable_periph_clk(BUTTSTART_PIO_ID);
+	 // configura pino ligado ao bot�o como entrada com um pull-up.
+	 pio_set_input(BUTTSTART_PIO,BUTTSTART_PIO_IDX_MASK,PIO_DEFAULT);
+	 // ativa pullup
+	 pio_pull_up(BUTTSTART_PIO, BUTTSTART_PIO_IDX_MASK, 1);
+	 //next
+	 pmc_enable_periph_clk(BUTTNEXT_PIO_ID);
+	 // configura pino ligado ao bot�o como entrada com um pull-up.
+	 pio_set_input(BUTTNEXT_PIO,BUTTNEXT_PIO_IDX_MASK,PIO_DEFAULT);
+	 // ativa pullup
+	 pio_pull_up(BUTTNEXT_PIO, BUTTNEXT_PIO_IDX_MASK, 1);
+}
+
+void play(int i,int duration[],int notes[]){
+	int aciona = notes[i]*(((float) duration[i])/1000.0);
+	for(int w=aciona;w>=0;w--)
+	{
+		pio_set(PIOA, BUZZ_PIO_IDX_MASK);
+		delay_us(500000/notes[i]);
+		pio_clear(PIOA, BUZZ_PIO_IDX_MASK);
+		delay_us(500000/notes[i]);
+	}
 }
 
 /************************************************************************/
@@ -64,28 +97,46 @@ void init(void)
 /************************************************************************/
 
 // Funcao principal chamada na inicalizacao do uC.
-int main(void)
-{
+int main(void){
   init();
+  int n=0;
 
   while(1){
+	if(pio_get(BUTTSTART_PIO,PIO_INPUT ,BUTTSTART_PIO_IDX_MASK)==0){
+		int *playing_notes = piratas;
+		int *playing_dura = dur_piratas;
+		pio_set(LED_PIO,LED_PIO_IDX_MASK);
 
-		  for (int i=0;i<sizeof(piratas)/sizeof(piratas[0]);i++)
-		  {
-			  int aciona = piratas[i]*(((float) dur_piratas[i])/1000.0);
-			  for(int w=aciona;w>=0;w--)
-			  {
-				  pio_set(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
-				  delay_us(500000/piratas[i]);
-				  pio_clear(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
-				  delay_us(500000/piratas[i]);
-				  pio_set(LED_PIO,LED_PIO_IDX_MASK);
-			  }
-			  pio_clear(LED_PIO,LED_PIO_IDX_MASK);
-		 }
+		  for (int i=0;i<sizeof(piratas)/sizeof(piratas[0]);i++){
+			if(pio_get(BUTTNEXT_PIO,PIO_INPUT ,BUTTNEXT_PIO_IDX_MASK)==0){
+				i=0;
+				delay_us(1);
 
-		//pio_clear(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
-		delay_s(2);
-  }
-  return 0;
+				if(n%2==0){
+					playing_notes = notes_2;
+					playing_dura = duration_2;
+					pio_clear(LED_PIO,LED_PIO_IDX_MASK);
+				}
+				
+				else{
+					playing_notes = piratas;
+					playing_dura = dur_piratas;
+					pio_set(LED_PIO,LED_PIO_IDX_MASK);
+				}
+				
+				n++;
+			}
+			play(i,playing_dura,playing_notes);
+		}
+	}
+ 			
+		else{
+			pio_clear(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
+		}
+	}
 }
+// 
+// 		pio_clear(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
+// 		delay_s(2);
+		
+		
