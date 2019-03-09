946b1bfafd8a5e8a9e9c427f88c7c0f2ab2f6ec5
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2019-03-09 13:57:55 -0300

    buzzer funcionando e organizacao dos arquivos

--- src/main.c
+++ src/main.c
@@ -1,23 +1,19 @@
 /**
- * 5 semestre - Eng. da Computação - Insper
- * Rafael Corsi - rafael.corsi@insper.edu.br
- *
- * Projeto 0 para a placa SAME70-XPLD
- *
- * Objetivo :
- *  - Introduzir ASF e HAL
- *  - Configuracao de clock
- *  - Configuracao pino In/Out
- *
- * Material :
- *  - Kit: ATMEL SAME70-XPLD - ARM CORTEX M7
- */
+ * 5 semestre - Eng. da Computaï¿½ï¿½o - Insper
+ /**
+  ** Entrega realizada em parceria com:
+  **  - Gabriel Monteiro - gabriellm1@al.insper.edu.br
+  **  - Guilherme Leite -
+  **
+  **  - url vÃ­deo
+  **/
 
 /************************************************************************/
 /* includes                                                             */
 /************************************************************************/
 
 #include "asf.h"
+#include "musics.h"
 
 /************************************************************************/
 /* defines                                                              */
@@ -27,160 +23,40 @@
 #define BUZZ_PIO_ID        ID_PIOA
 #define BUZZ_PIO_IDX       19u
 #define BUZZ_PIO_IDX_MASK  (1u << BUZZ_PIO_IDX)
+// Configurando Led
+#define LED_PIO           PIOC                  // periferico que controla o LED
+#define LED_PIO_ID        12                    // ID do perifï¿½rico PIOC (controla LED)
+#define LED_PIO_IDX       8u                    // ID do LED no PIO
+#define LED_PIO_IDX_MASK  (1u << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
 
-//defines musica(piratas do caribe)
-#define NOTE_C4  262   //Defining note frequency
-#define NOTE_D4  294
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_G4  392
-#define NOTE_A4  440
-#define NOTE_B4  494
-#define NOTE_C5  523
-#define NOTE_D5  587
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_G5  784
-#define NOTE_A5  880
-#define NOTE_B5  988
-
-/************************************************************************/
-/* constants                                                            */
-/************************************************************************/
-/************************************************************************/
-//*****************************************
-int notes[] = {       //Note of the song, 0 is a rest/pulse
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-   
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-   NOTE_C5, NOTE_A4, NOTE_B4, 0,
-
-   NOTE_A4, NOTE_A4, 
-   //Repeat of first part
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
-   NOTE_A4, NOTE_G4, NOTE_A4, 0,
-   
-   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
-   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
-   
-   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0, 
-   NOTE_D5, NOTE_E5, NOTE_A4, 0, 
-   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
-   NOTE_C5, NOTE_A4, NOTE_B4, 0,
-   //End of Repeat
-
-   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
-
-   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
-   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
-   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
-   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
-};
-//*****************************************
-int duration[] = {         //duration of each note (in ms) Quarter Note is set to 250 ms
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 125, 250, 125,
-
-  125, 125, 250, 125, 125, 
-  250, 125, 250, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 375, 375,
-
-  250, 125,
-  //Rpeat of First Part
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 375, 125, 
-  
-  125, 125, 250, 125, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 250, 125, 125,
-  125, 125, 125, 250, 125,
-
-  125, 125, 250, 125, 125, 
-  250, 125, 250, 125, 
-  125, 125, 250, 125, 125,
-  125, 125, 375, 375,
-  //End of Repeat
-  
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 125, 125, 125, 375,
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 500,
-
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 125, 125, 125, 375,
-  250, 125, 375, 250, 125, 375,
-  125, 125, 125, 125, 125, 500
-};
 
 void init(void);
-
-/************************************************************************/
-/* interrupcoes                                                         */
-/************************************************************************/
 
 /************************************************************************/
 /* funcoes                                                              */
 /************************************************************************/
 
-// Função de inicialização do uC
+
+// Funï¿½ï¿½o de inicializaï¿½ï¿½o do uC
 void init(void)
 {
 	  // Initialize the board clock
 	  sysclk_init();
-	  
+
 	  // Desativa WatchDog Timer
 	  WDT->WDT_MR = WDT_MR_WDDIS;
-	  
+
 	  // Ativa o PIO na qual o BUZZ foi conectado
 	  // para que possamos controlar o BUZZ.
 	  pmc_enable_periph_clk(BUZZ_PIO_ID);
-	  //Inicializa PA19 como saída
+	  //Inicializa PA19 como saï¿½da
 	  pio_set_output(BUZZ_PIO, BUZZ_PIO_IDX_MASK, 1, 0, 0);
+
+	    // Ativa o PIO na qual o LED foi conectado
+	    // para que possamos controlar o LED.
+	    pmc_enable_periph_clk(LED_PIO_ID);
+	    //Inicializa PC8 como saï¿½da
+	    pio_set_output(LED_PIO, LED_PIO_IDX_MASK, 1, 0, 0);
 }
 
 /************************************************************************/
@@ -192,31 +68,24 @@ int main(void)
 {
   init();
 
-  // super loop
-  // aplicacoes embarcadas não devem sair do while(1).
-  //while (1)
-  //{
-		for (int i=0;i<sizeof(notes);i++)
-		{
-		//	for (int j=0;j<duration;j++)
-			//{
-				int aciona = notes[i]*(((float) duration[i])/1000.0);
-				for(int w=aciona;w>=0;w--)
-				{
-					pio_set(PIOA, BUZZ_PIO_IDX_MASK);
-					delay_us(500000/notes[i]);
-					pio_clear(PIOA, BUZZ_PIO_IDX_MASK);
-					delay_us(500000/notes[i]);
-				}
-									pio_clear(PIOA, BUZZ_PIO_IDX_MASK);
-
-				
-				
-			}
-		//}
- // }
-  
   while(1){
+
+		  for (int i=0;i<sizeof(piratas)/sizeof(piratas[0]);i++)
+		  {
+			  int aciona = piratas[i]*(((float) dur_piratas[i])/1000.0);
+			  for(int w=aciona;w>=0;w--)
+			  {
+				  pio_set(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
+				  delay_us(500000/piratas[i]);
+				  pio_clear(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
+				  delay_us(500000/piratas[i]);
+				  pio_set(LED_PIO,LED_PIO_IDX_MASK);
+			  }
+			  pio_clear(LED_PIO,LED_PIO_IDX_MASK);
+		 }
+
+		//pio_clear(BUZZ_PIO, BUZZ_PIO_IDX_MASK);
+		delay_s(2);
   }
   return 0;
 }
---
+++ src/musics.h
@@ -0,0 +1,71 @@
+#include<notes.h>
+//*****************************************
+// PIRATAS DO CARIBE
+int piratas[] = {       //Note of the song, 0 is a rest/pulse
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+   NOTE_C5, NOTE_D5, NOTE_B4, NOTE_B4, 0,
+   NOTE_A4, NOTE_G4, NOTE_A4, 0,
+
+   NOTE_E4, NOTE_G4, NOTE_A4, NOTE_A4, 0,
+   NOTE_A4, NOTE_C5, NOTE_D5, NOTE_D5, 0,
+   NOTE_D5, NOTE_E5, NOTE_F5, NOTE_F5, 0,
+   NOTE_E5, NOTE_D5, NOTE_E5, NOTE_A4, 0,
+
+   NOTE_A4, NOTE_B4, NOTE_C5, NOTE_C5, 0,
+   NOTE_D5, NOTE_E5, NOTE_A4, 0,
+   NOTE_A4, NOTE_C5, NOTE_B4, NOTE_B4, 0,
+   NOTE_C5, NOTE_A4, NOTE_B4, 0,
+
+   NOTE_A4, NOTE_A4,
+
+   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4,
+
+   NOTE_E5, 0, 0, NOTE_F5, 0, 0,
+   NOTE_E5, NOTE_E5, 0, NOTE_G5, 0, NOTE_E5, NOTE_D5, 0, 0,
+   NOTE_D5, 0, 0, NOTE_C5, 0, 0,
+   NOTE_B4, NOTE_C5, 0, NOTE_B4, 0, NOTE_A4
+};
+int dur_piratas[] = {
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125,
+
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 125,
+
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 125, 250, 125,
+
+  125, 125, 250, 125, 125,
+  250, 125, 250, 125,
+  125, 125, 250, 125, 125,
+  125, 125, 375, 375,
+
+  250, 125,
+
+
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 125, 125, 125, 375,
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 500,
+
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 125, 125, 125, 375,
+  250, 125, 375, 250, 125, 375,
+  125, 125, 125, 125, 125, 500
+};
+//*****************************************
